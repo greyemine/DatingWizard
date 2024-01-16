c653bd1e0263a134465bfd5aa1ef8987ea04da3d
greyemine <156417251+greyemine@users.noreply.github.com>
2024-01-16 15:47:39 +0300

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-token = '6791984514:AAEBhngxcbNmldZDZ7i0B7U527xpumZCXuc'
-channel_name = '@club2_service_dating'
-
-inner_chat_name = '-1001342417108'
-test_inner_chat_name = '-1001289319696'
-
-message_thread_id = 1989
-test_message_thread_id = 956
